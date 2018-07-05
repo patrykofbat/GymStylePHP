e70514de78dfc17a2d2b540ef9bb5decbdca419b
Patryk <patrykpapior1997@gmail.com>
2018-07-05 19:59:51 +0200

    set up first endpoint

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-phpinfo();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header('Content-Type: application/json');
+
+$myArray = [
+    id=>"1001",
+    title=>"elo1",
+    link=>"elo1",
+    img=>"elo",
+    description=>"123"
+
+];
+
+$ar = array($myArray);
+echo json_encode($ar);
 
 ?>
